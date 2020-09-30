e69d5d67964e2763104e9b421ae59e107944309a
Kritik Garg <kritikgarg123@gmail.com>
2020-09-30 17:34:22 +0530

    airplane mode

---
+++ lib/database.dart
@@ -0,0 +1,45 @@
+class Similar {
+  double offset;
+  String url;
+  String name;
+  Similar(this.name, this.url, this.offset);
+
+}
+
+var cfx={'6921815609353':"6.4",
+  '8906009459943':"3.2",
+'8901207025716':"5.2",
+};
+
+var similar = {
+  '6921815609353': [
+    Similar(
+        "Samsung ITFIT-A08B",
+        "https://www.reliancedigital.in/medias/Samsung-ITFIT-Bluetooth-Stereo-Earphone-A08B-Black-491895039-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w4ODE1M3xpbWFnZS9qcGVnfGltYWdlcy9oY2UvaDY0LzkzMzM0NjM0ODIzOTguanBnfGVkMGVkMWJhMDJjMzM4MWY2MWJjOGEzNTA3NjJhMDk2NjM5OTk5MGQ2ODA5MzRiMDhhMmM4ZWQ1MTViZjI2MDQ",
+        -0.6),
+    Similar(
+        "boAt Rockerz 240",
+        "https://rukminim1.flixcart.com/image/416/416/k4k7f680/headphone/e/j/8/boat-rockerz-240-rockerz-239-original-imafn86zasbj4sgm.jpeg?q=70",
+        -0.3),
+  ],
+  '8906009459943': [
+    Similar(
+        "Himalaya Herbals Purifying Neem",
+        "https://5.imimg.com/data5/QB/KD/MY-20033121/himalaya-herbals-purifying-neem-face-wash-2c-150ml-500x500.png",
+        -1.1),
+    Similar(
+        "Botanical Lab Neem Leaf ",
+        "https://www.superdrug.com/medias/sys_master/front-zoom/front-zoom/h36/ha9/9693106339870/Botanical-Lab-Neem-Leaf-Gentle-Face-Wash-150ml-760284.jpg",
+        -0.92),
+  ],
+  '8901207025716': [
+    Similar(
+        "Patanjali Chyawanprash 1kg",
+        "https://images-eu.ssl-images-amazon.com/images/I/31vEU5nBNPL._AC_UL474_SR474,450_.jpg",
+        -1.3),
+    Similar(
+        "Himalayan Institute, Chyawanprash, 1.1 lbs",
+        "https://s3.images-iherb.com/hip/hip80010/w/14.jpg",
+        -0.5),
+  ]
+};
--- lib/product.dart
+++ lib/product.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'database.dart';
+
+
 String src =
     "https://i.ibb.co/6XjvnSV/kisspng-chocolate-bar-cadbury-dairy-milk-nut-dairy-5ad037c29ecdd5-1.png";
 
@@ -11,52 +14,247 @@ class Product extends StatelessWidget {
   final String name;
   final bool load;
 
-  Product(this.isbn, this.cf, this.imgUrl,this.name,this.load);
+  Product(this.isbn, this.cf, this.imgUrl, this.name, this.load);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          load?Container(width:130,height: 130,child:SpinKitWave(size: 30, color: Colors.teal,)):
-          Padding(
-            padding: const EdgeInsets.only(top:18.0,bottom: 18,left: 10,right: 10),
-            child: Container(width:130,height: 130,child: Image.network(imgUrl)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Column(mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("ISBN "+isbn,
-                    style: GoogleFonts.montserrat(fontWeight: FontWeight.w600,
-                        fontSize: 12, color: Colors.black87)),
-                SizedBox(height: 5,),
-                Container(width: 150,
-                  child: Text(name,maxLines: 2,overflow: TextOverflow.ellipsis,textAlign: TextAlign.center,
-                      style: GoogleFonts.montserrat(fontWeight: FontWeight.w600,
-                          fontSize: 13, color: Colors.black87)),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          child: Row(
+            children: [
+              load
+                  ? Container(
+                      width: 130,
+                      height: 130,
+                      child: SpinKitWave(
+                        size: 30,
+                        color: Colors.teal,
+                      ))
+                  : Padding(
+                      padding: const EdgeInsets.only(
+                          top: 18.0, bottom: 18, left: 10, right: 10),
+                      child: Container(
+                          width: 130,
+                          height: 130,
+                          child: Image.network(imgUrl)),
+                    ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("ISBN " + isbn,
+                        style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 12,
+                            color: Colors.black87)),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: 150,
+                      child: Text(name,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.montserrat(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 13,
+                              color: Colors.black87)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 2,
+                      color: Colors.black,
+                    ),
+                    SizedBox(height: 2),
+                    Text(cfx[isbn],
+                        style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 55,
+                            color: Colors.teal)),
+                    Text("Carbon footprint",
+                        style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                            color: Colors.black87)),
+                  ],
                 ),
-                SizedBox(height: 10,),
-                Container(width: 120,height:2,color: Colors.black,),
-                SizedBox(height: 2),
-                Text(cf.toString(),
-                    style: GoogleFonts.montserrat(fontWeight: FontWeight.w700,
-                        fontSize: 55, color: Colors.teal)),
-                Text("Carbon footprint",
-                    style: GoogleFonts.montserrat(fontWeight: FontWeight.w600,
-                        fontSize: 14, color: Colors.black87)),
-              ],
+              )
+            ],
+          ),
+          width: 310,
+          height: 190,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(255, 255, 255, 0.85),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 4.0,bottom: 3),
+          child: Container(
+            width: 300,
+            child: Center(
+              child: Text("Greener Alternatives",
+                  style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      color: Colors.white)),
             ),
-          )
-        ],
-      ),
-      width: 310,
-      height: 190,
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(255, 255, 255,0.85),
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-      ),
+          ),
+        ),
+        Container(
+          width: 290,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(2),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          load
+                              ? Container(
+                                  width: 85,
+                                  height: 100,
+                                  child: SpinKitWave(
+                                    size: 30,
+                                    color: Colors.teal,
+                                  ))
+                              : Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 5.0, bottom: 5, left: 3),
+                                  child: Container(
+                                      width: 85,
+                                      height: 100,
+                                      child:
+                                          Image.network(similar[isbn][0].url)),
+                                ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 3.0, top: 6.0),
+                            child: Text(similar[isbn][0].offset.toString(),
+                                style: GoogleFonts.montserrat(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 16,
+                                    color: Colors.teal)),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 2,
+                            ),
+                            Container(
+                              width: 120,
+                              child: Text(similar[isbn][0].name,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.montserrat(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 12,
+                                      color: Colors.black87)),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  width: 139,
+                  height: 160,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 0.85),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(2),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          load
+                              ? Container(
+                                  width: 90,
+                                  height: 100,
+                                  child: SpinKitWave(
+                                    size: 30,
+                                    color: Colors.teal,
+                                  ))
+                              : Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 5.0, bottom: 5, left: 4),
+                                  child: Container(
+                                      width: 90,
+                                      height: 100,
+                                      child:
+                                          Image.network(similar[isbn][1].url)),
+                                ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 3.0, top: 6.0),
+                            child: Text(similar[isbn][1].offset.toString(),
+                                style: GoogleFonts.montserrat(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 16,
+                                    color: Colors.teal)),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 2,
+                            ),
+                            Container(
+                              width: 120,
+                              child: Text(similar[isbn][1].name,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.montserrat(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 12,
+                                      color: Colors.black87)),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  width: 139,
+                  height: 160,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 0.85),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
